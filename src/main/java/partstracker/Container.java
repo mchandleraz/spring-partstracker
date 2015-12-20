93ebf9288886d122f5be327c5e09c618e1c53839
Matt Chandler <mcaz689@gmail.com>
2015-12-20 14:04:33 -0700

    add id back

--- src/main/java/partstracker/Container.java
+++ src/main/java/partstracker/Container.java
@@ -5,13 +5,24 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Container {
 
 	private final String label;
+	private final long id;
 
 	public Container(@JsonProperty("label") String label) {
 		this.label = label;
+		this.id = this.createId();
 	}
 
 	public String getLabel() {
 		return label;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	private long createId() {
+		long id = ContainerService.findAllContainers().size() + 1;
+		return id;
 	}
 
 }
